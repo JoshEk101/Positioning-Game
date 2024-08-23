a9467a320f1f6e2aae719f14fd07bacbd475f95d
JoshEk101 <118575438+JoshEk101@users.noreply.github.com>
2024-08-23 19:36:38 +0100

    Added turn-based moving

--- usingGame.py
+++ usingGame.py
@@ -1,6 +1,7 @@
 import pygame
 import sys
 import random
+import time
 
 SCREEN_WIDTH = 750
 SCREEN_HEIGHT = 600
@@ -32,15 +33,14 @@ def check_interception(A, B, C):
     else:
         return False
 
-
 def startingCoordinates():
     playerOne = [random.choice([150, 300, 450, 600]), (random.choice([100, 200, 300, 400]))]
     playerTwo = [random.choice([150, 300, 450, 600]), random.choice([100, 200, 300, 400])]
     while playerTwo == playerOne:
         playerTwo = [random.choice([150, 300, 450, 600]), random.choice([100, 200, 300, 400])]
-    stone = [random.choice([150, 300, 450, 600]), random.choice([100, 200, 300, 400])]
+    stone = [random.choice([300, 450]), random.choice([200, 300])]
     while stone == playerOne or stone == playerTwo:
-        stone = [random.choice([150, 300, 450, 600]), random.choice([100, 200, 300, 400])]
+        stone = [random.choice([300, 450]), random.choice([200, 300])]
     return playerOne, playerTwo, stone
 
 def createBoard():
@@ -48,10 +48,10 @@ def createBoard():
     screen.fill(COLOUR)
 
     # vertical lines
-    pygame.draw.line(screen, COLOUR_LINE, (150, 0), (150, 500), 5)
-    pygame.draw.line(screen, COLOUR_LINE, (300, 0), (300, 500), 5)
-    pygame.draw.line(screen, COLOUR_LINE, (450, 0), (450, 500), 5)
-    pygame.draw.line(screen, COLOUR_LINE, (600, 0), (600, 500), 5)
+    pygame.draw.line(screen, COLOUR_LINE, (150, 0), (150, 400), 5)
+    pygame.draw.line(screen, COLOUR_LINE, (300, 0), (300, 400), 5)
+    pygame.draw.line(screen, COLOUR_LINE, (450, 0), (450, 400), 5)
+    pygame.draw.line(screen, COLOUR_LINE, (600, 0), (600, 400), 5)
 
     # horizontal lines
     pygame.draw.line(screen, COLOUR_LINE, (0, 100), (750, 100), 5)
@@ -66,12 +66,10 @@ def createBoard():
 
     return screen
 
-def checkCollision(playerOne, playerTwo, stone):
-    if playerOne == playerTwo or playerOne == stone or playerTwo == stone:
+def checkCollision(movingPlayer, stationaryPlayer, stone):
+    if movingPlayer == stationaryPlayer or movingPlayer == stone or stationaryPlayer == stone:
         return True
-    if playerOne[0] > 600 or playerOne[1] > 400 or playerOne[0] < 150 or playerOne[1] < 100:
-        return True
-    if playerTwo[0] > 600 or playerTwo[1] > 400 or playerTwo[0] < 150 or playerTwo[1] < 100:
+    if movingPlayer[0] > 600 or movingPlayer[1] > 400 or movingPlayer[0] < 150 or movingPlayer[1] < 100:
         return True
     return False
 
@@ -107,37 +105,114 @@ class Player():
     def __init__(self, x, y, image, scale):
         width = image.get_width()
         height = image.get_height()
-        self.image = pygame.transform.scale(image, (int(width * 3), int(height * 3)))
+        self.image = pygame.transform.scale(image, (int(width * scale), int(height * scale)))
         self.rect = self.image.get_rect()
         self.rect.topleft = (x, y)
+        self.health = 100
+        self.can_fire = False
+        self.moves_left = 3
+        self.bullet_amount = 1
 
     def draw(self):
-        screen.blit(self.image, (self.rect.x, self.rect.y))
+        screen.blit(self.image, self.image.get_rect(center = (self.rect.x, self.rect.y)))
 
     def move(self, direction):
         if direction == 'left':
             self.rect.x -= 150
-            if checkCollision([self.rect.x, self.rect.y], playerTwoCoordinates, stoneCoordinates):
+            if checkCollision([self.rect.x, self.rect.y], computer.getComputerCoordinates(), stoneCoordinates):
                 self.rect.x += 150
+            elif self.moves_left > 0:
+                self.moves_left -= 1
         elif direction == 'right':
             self.rect.x += 150
-            if checkCollision([self.rect.x, self.rect.y], playerTwoCoordinates, stoneCoordinates):
+            if checkCollision([self.rect.x, self.rect.y], computer.getComputerCoordinates(), stoneCoordinates):
                 self.rect.x -= 150
+            elif self.moves_left > 0:
+                self.moves_left -= 1
         elif direction == 'up':
             self.rect.y -= 100
-            if checkCollision([self.rect.x, self.rect.y], playerTwoCoordinates, stoneCoordinates):
+            if checkCollision([self.rect.x, self.rect.y], computer.getComputerCoordinates(), stoneCoordinates):
                 self.rect.y += 100
+            elif self.moves_left > 0:
+                self.moves_left -= 1
         elif direction == 'down':
             self.rect.y += 100
-            if checkCollision([self.rect.x, self.rect.y], playerTwoCoordinates, stoneCoordinates):
+            if checkCollision([self.rect.x, self.rect.y], computer.getComputerCoordinates(), stoneCoordinates):
                 self.rect.y -= 100
+            elif self.moves_left > 0:
+                self.moves_left -= 1
         
-    def get_image(self):
-        return (self.image)
+    def get_moves(self):
+        return self.moves_left
     
-    def get_position(self):
-        return (self.rect.topleft)
-        
+    def reset_moves(self):
+        self.moves_left = 3
+
+    def get_coordinates(self):
+        return [self.rect.x, self.rect.y]
+
+class Computer():
+    def __init__(self, x, y, image, scale):
+        width = image.get_width()
+        height = image.get_height()
+        self.image = pygame.transform.scale(image, (int(width * scale), int(height * scale)))
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x, y)
+        self.health = 100
+        self.can_fire = False
+        self.moves_left = 3
+        self.bullet_amount = 1
+
+    def draw(self):
+        screen.blit(self.image, self.image.get_rect(center = (self.rect.x, self.rect.y)))
+
+    def move(self, playerOnePosition, stonePosition):
+        moves = ['l', 'r', 'u', 'd']
+        moving = 3
+        while moving > 0:
+            next_move = random.choice(moves)
+            if next_move == 'l':
+                if not checkCollision([(self.rect.x-150), self.rect.y], playerOnePosition, stonePosition):
+                    self.rect.x -= 150
+                    moving -= 1
+                    moves = ['l', 'r', 'u', 'd']
+                else:
+                    moves = ['r', 'u', 'd']
+            elif next_move == 'r':
+                if not checkCollision([(self.rect.x+150), self.rect.y], playerOnePosition, stonePosition):
+                    self.rect.x += 150
+                    moving -= 1
+                    moves = ['l', 'r', 'u', 'd']
+                else:
+                    moves = ['l', 'u', 'd']
+            elif next_move == 'u':
+                if not checkCollision([self.rect.x, (self.rect.y-100)], playerOnePosition, stonePosition):
+                    self.rect.y -= 100
+                    moving -= 1
+                    moves = ['l', 'r', 'u', 'd']
+                else:
+                    moves = ['l', 'r', 'd']
+            elif next_move == 'd':
+                if not checkCollision([self.rect.x, (self.rect.y+100)], playerOnePosition, stonePosition):
+                    self.rect.y += 100
+                    moving -= 1
+                    moves = ['l', 'r', 'u', 'd']
+                else:
+                    moves = ['l', 'r', 'u']
+            createBoard()
+            if exit_button.draw():
+                pygame.quit()
+                sys.exit()
+            self.draw()
+            playerOne.draw()
+            pygame.draw.circle(screen, STONE_COLOUR, stonePosition, 10)
+            pygame.display.update()
+            time.sleep(1)
+        pygame.event.clear()
+        playerOne.reset_moves()
+
+    def getComputerCoordinates(self):
+        return [self.rect.x, self.rect.y]
 
 pygame.init()
 # note that 0, 0 is the top left of the screen
@@ -148,9 +223,11 @@ playerOneCoordinates, playerTwoCoordinates, stoneCoordinates = startingCoordinat
 playerOne = Player(playerOneCoordinates[0], playerOneCoordinates[1], pygame.image.load("Terry.png"), 3)
 
 # playerOne = pygame.draw.circle(screen, PLAYER_ONE_COLOUR, playerOneCoordinates, 10)
-playerTwo = pygame.draw.circle(screen, PLAYER_TWO_COLOUR, playerTwoCoordinates, 10)
+# playerTwo = pygame.draw.circle(screen, PLAYER_TWO_COLOUR, playerTwoCoordinates, 10)
+computer = Computer(playerTwoCoordinates[0], playerTwoCoordinates[1], pygame.image.load("Enemy.png"), 3)
+
 #blue
-playerThree = pygame.draw.circle(screen, STONE_COLOUR, stoneCoordinates, 10)
+stone = pygame.draw.circle(screen, STONE_COLOUR, stoneCoordinates, 10)
 #green
 title = check_interception(playerOneCoordinates, playerTwoCoordinates, stoneCoordinates)
 pygame.display.set_caption(str(title))
@@ -166,7 +243,8 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-        if event.type == pygame.KEYDOWN:
+        if event.type == pygame.KEYDOWN and playerOne.get_moves() > 0:
+            print("doing")
             if event.key == pygame.K_DOWN:
                 playerOne.move('down')
             elif event.key == pygame.K_UP:
@@ -176,9 +254,11 @@ while run:
             elif event.key == pygame.K_LEFT:
                 playerOne.move('left')
             screen = createBoard()
-    playerTwo = pygame.draw.circle(screen, PLAYER_TWO_COLOUR, playerTwoCoordinates, 10)
-    playerThree = pygame.draw.circle(screen, STONE_COLOUR, stoneCoordinates, 10)
-    screen.blit(playerOne.get_image(), playerOne.get_position())
+    computer.draw()
+    stone = pygame.draw.circle(screen, STONE_COLOUR, stoneCoordinates, 10)
+    playerOne.draw()
+    if playerOne.get_moves() == 0:
+        computer.move(playerOne.get_coordinates(), stoneCoordinates)
     pygame.display.flip()
     pygame.display.update()
 
